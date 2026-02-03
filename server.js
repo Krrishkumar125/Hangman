110b12fdd99d345c2f05df163b3f426902fbe077
Krrishkumar125 <krrishkumar2028@gmail.com>
2026-02-04 02:57:24 +0530

    Feat: Added mongo connection

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const app = require('./src/app');
 const http = require('http');
 const config = require('./src/config/server.config');
 const logger = require('./src/utils/logger');
+const { connectDB } = require('./src/config/db.config');
 
 const server = http.createServer(app);
+
+connectDB();
 
 server.listen(config.port, () => {
   logger.info(`Server running in ${config.nodeEnv} mode on port ${config.port}`);
